525f9caa17a0b4ebc8203a73b062aacf91fd7dc8
siphomsiza <siphosmall@gmail.com>
2014-03-26 16:57:53 +0200

    adding scopes for model Vehicle [#105]

--- lib/glassguide/vehicle.rb
+++ lib/glassguide/vehicle.rb
@@ -7,30 +7,32 @@ module Glassguide
       self[:code] = val
     end
 
-    # named_scope :years, :select => 'DISTINCT(`glasses_vehicles`.`year`)', :order => 'year DESC'
+    #scope :motorcycle, -> { where(motorcycle: true) }
+    # named_scope :years, :select => 'DISTINCT(`glassguide_vehicles`.`year`)', :order => 'year DESC'
 
     def self.years
       find(:all, :group => 'year', :select => 'year')
     end
 
-    # scope :makes, :select => 'DISTINCT(`glasses_vehicles`.`make`)', :order => 'make ASC'
-    # scope :families, :select => 'DISTINCT(`glasses_vehicles`.`family`)', :order => 'family ASC'
-    # scope :variants, :select => 'DISTINCT(`glasses_vehicles`.`variant`)', :order => 'variant ASC'
-    # scope :styles, :select => 'DISTINCT(`glasses_vehicles`.`style`)', :order => 'style ASC'
-    # scope :transmissions, :select => 'DISTINCT(`glasses_vehicles`.`transmission`)', :order => 'transmission ASC'
-    # scope :series, :select => 'DISTINCT(`glasses_vehicles`.`series`)', :order => 'series ASC'
-    # scope :engines, :select => 'DISTINCT(`glasses_vehicles`.`engine`), `glasses_vehicles`.`size`, `glasses_vehicles`.`cyl`', :order => 'engine ASC'
+    #scope :makes, :select => 'DISTINCT(`glassguide_vehicles`.`make`)', :order => 'make ASC'
+    #scope :families, :select => 'DISTINCT(`glassguide_vehicles`.`family`)', :order => 'family ASC'
+    #scope :variants, :select => 'DISTINCT(`glassguide_vehicles`.`variant`)', :order => 'variant ASC'
+    #scope :styles, :select => 'DISTINCT(`glassguide_vehicles`.`style`)', :order => 'style ASC'
+    #scope :transmissions, :select => 'DISTINCT(`glassguide_vehicles`.`transmission`)', :order => 'transmission ASC'
+    #scope :series, :select => 'DISTINCT(`glassguide_vehicles`.`series`)', :order => 'series ASC'
+    #scope :engines, :select => 'DISTINCT(`glassguide_vehicles`.`engine`), `glassguide_vehicles`.`size`, `glassguide_vehicles`.`cyl`', :order => 'engine ASC'
     # # By scopes
 
-
-    # scope :by_year, lambda { |year| { :conditions => (year == 'New') ? ['price_new IS NOT NULL'] : ['year = ? ', year] } }
-    # scope :by_make, lambda { |make| { :conditions => {:make => make} } }
-    # scope :by_family, lambda { |family| { :conditions => {:family => family} } }
-    # scope :by_variant, lambda { |variant| { :conditions => {:variant => variant} } }
-    # scope :by_style, lambda { |style| { :conditions => {:style => style} } }
-    # scope :by_transmission, lambda { |transmission| { :conditions => {:transmission => transmission} } }
-    # scope :by_series, lambda { |series| { :conditions => {:series => series} } }
-    # scope :by_engine, lambda { |engine| { :conditions => {:engine => engine} } }
+    
+    scope :by_year ,->(year) {where((year == 'New') ? ['price_new IS NOT NULL'] : ['year = ? ', year])}
+    scope :by_make ,->(make) {where(:make => make)}    
+    scope :by_family ,->(family) {where("family=?", family)}    
+    scope :by_variant ,->(variant) {where(:variant => variant)}    
+    scope :by_style ,->(style) {where(:style => style)}    
+    scope :by_transmission ,->(transmission) {where(:transmission => transmission)}    
+    scope :by_family ,->(series) {where(:series => series)}    
+    scope :by_engine ,->(engine) {where(:engine => engine)}  
+    
     # scope :by_vehicle_type, lambda { |vehicle_type| { :conditions => {:motorcycle => (vehicle_type.downcase == 'motorcycle')} } }
 
     # has_one :standard_options, :class_name => "Glasses::Option", :foreign_key => "vehicle_code", :conditions => {:rec_type => 'Standard:'}

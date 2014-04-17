1078ea6733763e25eb2f2fb4b40f5392c21cae15
Daniel <SoetSout@gmail.com>
2014-04-17 09:45:01 +0200

    fix

--- lib/glassguide/vehicle.rb
+++ lib/glassguide/vehicle.rb
@@ -152,7 +152,7 @@ module Glassguide
         depreciation_table = {:new_vehicle    => true,
                               :price_new      => self.price_new,
                               :options_amount => (self.value_of_options(option_codes) rescue 0),
-                              :adjusted_value => (self.price_new + self.value_of_options(option_codes)
+                              :adjusted_value => (self.price_new + self.value_of_options(option_codes))
                             }
       else
         depreciation_table = {
